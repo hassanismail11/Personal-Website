d2a5b116ea0fe08db53c3f3ec977a6df9ce1da1c
hassanismail11 <hihassanismail11@gmail.com>
2023-10-12 04:41:43 +0300

    Pre-Deploy

--- app.js
+++ app.js
@@ -1,61 +1,89 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
-app.set('views', (__dirname + "/views"));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/" + "myblog");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// Replace with your Atlas connection string
+const uri = process.env.CLIENT_URI;
 
-// Posts DataBase
-const postSchema = new mongoose.Schema({
-  postTitle: String,
-  postURL: String,
-  postContent: String,
-  postDate: String,
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-const Post = mongoose.model("Post", postSchema);
-function newPost(postTitle, postContent, postDate) {
-  const newPost = new Post({
-    postTitle: postTitle,
-    postURL: _.kebabCase(_.lowerCase(postTitle)),
-    postContent: postContent,
-    postDate: postDate,
-  });
+let Posts = [];
+let post = {};
 
-  newPost.save();
-  console.log("New Post Added");
+async function findPosts(_callback) {
+  try {
+    await client.connect();
+
+    Posts = await client
+      .db("personal-website")
+      .collection("posts")
+      .find()
+      .toArray();
+  } finally {
+    _callback();
+  }
 }
 
-// Work Database
-const workSchema = new mongoose.Schema({
-  workTitle: String,
-  workURL: String,
-  workContent: String,
-  workImageURL: String,
-  workDate: String
-});
+async function findPostByURL(postURL, _callback) {
+  try {
+    await client.connect();
 
-const Work = mongoose.model("Work", workSchema);
-function newWork(workTitle, workContent, workImageURL, workDate) {
-  const newWork = new Work({
-    workTitle: workTitle,
-    workURL: _.kebabCase(_.lowerCase(workTitle)),
-    workContent: workContent,
-    workImageURL: workImageURL,
-    workDate: workDate
-  });
+    post = await client
+      .db("personal-website")
+      .collection("posts")
+      .findOne({postURL:postURL});
 
-  newWork.save();
-  console.log("New Work Added");
+  } finally {
+    _callback();
+  }
+}
+
+let Works = [];
+let work = {};
+
+async function findWorks(_callback) {
+  try {
+    await client.connect();
+
+    Works = await client
+      .db("personal-website")
+      .collection("works")
+      .find()
+      .toArray();
+  } finally {
+    _callback();
+  }
+}
+
+async function findWorkByURL(workURL, _callback) {
+  try {
+    await client.connect();
+
+    work = await client
+      .db("personal-website")
+      .collection("works")
+      .findOne({workURL:workURL});
+
+  } finally {
+    _callback();
+  }
 }
 
 function getDate() {
@@ -79,27 +107,17 @@ app.get("/", function (req, res) {
 /* My Work Page */
 
 app.get("/mywork", function (req, res) {
-  Work.find({})
-    .then((workPosts) => res.render("mywork", { works: workPosts }));
-});
-
-/* Compose work Page */
-
-app.get("/composeWork", function (req, res) {
-  res.render("composeWork");
-});
-
-app.post("/composeWork", function (req, res) {
-  newWork();
-  res.redirect("/mywork");
+  findWorks(function () {
+    res.render("mywork", { works: Works });
+  });
 });
 
 /* Work Page */
 
-app.get("/work/:workTitle", function (req, res) {
-  Work.find({ workURL: req.params.workTitle }).then((foundWork) =>
-    res.render("work", { workPage: foundWork })
-  );
+app.get("/myworks/:workTitle", function (req, res) {
+  findWorkByURL(req.params.workTitle, function(){
+    res.render("work", { work: work })
+  })
 });
 
 /* About Page */
@@ -121,29 +139,24 @@ app.post("/contact", function (req, res) {
 /* Blog Page */
 
 app.get("/blog", function (req, res) {
-  Post.find({})
-    .sort({ postTitle: 1 })
-    .then((blogPosts) => res.render("blog", { posts: blogPosts }));
-});
-
-/* Compose Post Page */
-
-app.get("/compose", function (req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function (req, res) {
-  newPost(req.body.postTitle, req.body.postText, getDate());
-  res.redirect("/");
+  findPosts(function () {
+    res.render("blog", { posts: Posts });
+  });
 });
 
 /* Post Page */
 
 app.get("/posts/:posttitle", function (req, res) {
-  Post.find({ postURL: req.params.posttitle }).then((foundPost) =>
-    res.render("post", { postPage: foundPost })
-  );
+  findPostByURL(req.params.posttitle, function(){
+    res.render("post", { postPage: post })
+  })
 });
+
+/* 404 NOT FOUND */
+
+app.use((req, res, next) => { 
+  res.status(404).render("notfound");
+}) 
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
